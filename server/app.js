421960697aae7251ae67036126d7901a3d428c47
Aluno <aluno@decom-OptiPlex-780>
2016-05-12 12:15:59 -0300

    pegar os mais jogados

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     fs = require('fs'),
-    hbs = require('hbs');
+    hbs = require('hbs'),
+    _ = require('underscore');
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
@@ -28,12 +29,21 @@ app.get('/', function(request, response) {
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter umas 15 linhas de código
-app.get('/jogadores/:id', function(request, response) {
-  response.render('jogador', {
-    profile: jogadores[id]/*,
-    gameInfo: jogosDesteJogador,
-    favorito: jogosDesteJogador[0]*/
+app.get('/jogador/:id', function(request, response) {
+  var jogador = _.findWhere(db.jogadores, {steamid: request.params.id});
+  //console.log(db.jogosPorJogador);
+  var jogJogador = db.jogosPorJogador[request.params.id];
+  var jogosTodosJogador = jogJogador.games;
+  jogador.qntJogos = jogJogador.game_count;
+  var naoJogados = _.where(jogosTodosJogador, { playtime_forever: 0 })
+  jogador.qntNJogados = naoJogados.length;
+
+  _.sortBy(jogosTodosJogador, function(jogo){
+      return jogo.playtime_forever;
   });
+
+  console.log(jogador);
+  //response.render('jogador', jogador);
 });
 
 // configurar para servir os arquivos estáticos da pasta "client"
